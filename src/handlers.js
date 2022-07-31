c216b9fcdc508d7539958758f8a43c3f29c0bfb5
Eric Xiong <exiong@ualberta.ca>
2022-07-31 10:56:38 -0600

    alpha/beta calibration

--- src/handlers.js
+++ src/handlers.js
@@ -9,7 +9,8 @@ let tp9Array = [0,0,0,0];
 let tp10Array = [0,0,0,0];
 let af7Array = [0,0,0,0];
 let af8Array = [0,0,0,0];
-
+let ABarray = [0,0]
+let channelCount = 0;
 
 export const ondata = (track, data, timestamp) => {
 
@@ -28,10 +29,15 @@ export const ondata = (track, data, timestamp) => {
 
     // update the text content of this track's paragraph
     let sum = 0;
-    for (let i = 0; i<12;i++){
-        sum =+ data[i]
+    if (channelCount == 40){
+        channelCount = 0
+        console.log(ABarray[1]/ABarray[0])
+        ABarray = [0,0]
     }
     if (label==='AF7'){
+        //for (let i=0;i<12;i++){
+          //  sum += data[i]
+        //}
         if (af7Array.length < 256){
             for (let i = 0; i<12; i++){
                 af7Array.push(parseInt(data[i]));
@@ -39,17 +45,24 @@ export const ondata = (track, data, timestamp) => {
         }
         else{
             //console.log(af7Array)
-            //console.log('AF7')
-            //console.log(fourier(af7Array))
+            console.log('AF7')
+            //[ABarray[0],ABarray[1]] =+ fourier(af7Array)
+            channelCount++
+            let[a,b] = fourier(af7Array)
+            ABarray[0] += a
+            ABarray[1] += b
+            af7Array = [0,0,0,0];
+
         }
         for (let i = 0; i<12;i++){
             if (data[i]>300){
                 //console.log('RAISE')
-    
-                if ((lastFire+1) < (timestamp[0] - startTime)/1000){
+
+                if ((lastFire+0.5) < (timestamp[0] - startTime)/1000){
                     body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16)
                 }
                 lastFire = ((timestamp[0] - startTime)/1000)
+                break
             }
         }
 
@@ -62,7 +75,23 @@ export const ondata = (track, data, timestamp) => {
         }
         else{
             //console.log(af8Array)
-            //console.log('AF8')
+            console.log('AF8')
+            let[a,b] = fourier(af8Array)
+            ABarray[0] += a
+            ABarray[1] += b
+            //[ABarray[0],ABarray[1]] =+ fourier(af8Array)
+            channelCount++
+            af8Array = [0,0,0,0];
+        }
+        for (let i = 0; i<12;i++){
+            if (data[i]>300){
+                //console.log('RAISE')
+    
+                if ((lastFire+0.5) < (timestamp[0] - startTime)/1000){
+                    body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+                }
+                lastFire = ((timestamp[0] - startTime)/1000)
+            }
         }
     }
     if (label==='TP9'){
@@ -73,7 +102,13 @@ export const ondata = (track, data, timestamp) => {
         }
         else{
             //console.log(tp9Array)
-            //console.log('TP9')
+            console.log('TP9')
+            //[ABarray[0],ABarray[1]] =+ fourier(tp9Array)
+            channelCount++
+            let[a,b] = fourier(tp9Array)
+            ABarray[0] += a
+            ABarray[1] += b
+            tp9Array = [0,0,0,0];
         }
     }
     if (label==='TP10'){
@@ -84,7 +119,12 @@ export const ondata = (track, data, timestamp) => {
         }
         else{
             //console.log(tp10Array)
-            //console.log('TP10')
+            console.log('TP10')
+            let [a,b] = fourier(tp10Array)
+            ABarray[0] += a
+            ABarray[1] += b
+            channelCount++
+            tp10Array = [0,0,0,0];
         }
     }
     //console.log(JSON.stringify(data));
